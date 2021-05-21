be7919737aee88134b3e6b4f86421fb46e3dee6d
woong <taewoong@gurum.cc>
2021-05-21 15:08:03 +0900

    DIFS: Add hc-key-chain & hc-segment-fetcher

---
+++ ndn-cxx/security/hc-key-chain.cpp
@@ -0,0 +1,51 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2013-2020 Regents of the University of California.
+ *
+ * This file is part of ndn-cxx library (NDN C++ library with eXperimental eXtensions).
+ *
+ * ndn-cxx library is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * ndn-cxx library is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received copies of the GNU General Public License and GNU Lesser
+ * General Public License along with ndn-cxx, e.g., in COPYING.md file.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * See AUTHORS.md for complete list of ndn-cxx authors and contributors.
+ */
+
+#include <iostream>
+#include <algorithm>
+
+#include "ndn-cxx/security/hc-key-chain.hpp"
+
+#include "ndn-cxx/interest.hpp"
+#include "ndn-cxx/security/certificate.hpp"
+#include "ndn-cxx/security/key-params.hpp"
+#include "ndn-cxx/security/pib/pib.hpp"
+#include "ndn-cxx/security/safe-bag.hpp"
+#include "ndn-cxx/security/signing-info.hpp"
+#include "ndn-cxx/security/tpm/tpm.hpp"
+
+namespace ndn {
+namespace security {
+inline namespace v2{
+void
+HCKeyChain::sign(Data &data, const ndn::Block &nextHash, const SigningInfo &params) {
+  auto content = data.getContent();
+  ndn::Block realcontent = ndn::encoding::makeBinaryBlock(ndn::tlv::Content, content.value(), content.value_size());
+  content.resetWire();
+  content.push_back(realcontent);
+  content.push_back(nextHash);
+  data.setContent(content);
+
+  KeyChain::sign(data, params);
+}
+}
+}
+}
---
+++ ndn-cxx/util/hc-segment-fetcher.cpp
@@ -0,0 +1,110 @@
+#include <ndn-cxx/util/hc-segment-fetcher.hpp>
+#include <iostream>
+
+namespace ndn {
+namespace util {
+
+HCSegmentFetcher::HCSegmentFetcher(Face& face, security::v2::Validator& validator, const SegmentFetcher::Options& options) {
+
+}
+
+shared_ptr<HCSegmentFetcher>
+HCSegmentFetcher::start(Face &face,
+    const Interest &baseInterest,
+    security::v2::Validator &validator,
+    const SegmentFetcher::Options &options)
+{
+  shared_ptr<HCSegmentFetcher> hc_fetcher(new HCSegmentFetcher(face, validator, options));
+
+  auto m_fetcher = SegmentFetcher::start(face, baseInterest, validator, options);
+
+  m_fetcher->onComplete.connect([hc_fetcher] (const ConstBufferPtr& buffer)
+                                         { hc_fetcher->onComplete(buffer);
+                                         });
+
+  m_fetcher->afterSegmentReceived.connect([hc_fetcher] (const Data& data)
+                                         { hc_fetcher->afterSegmentReceived(data);
+                                         });
+
+  m_fetcher->afterSegmentNacked.connect([hc_fetcher] ()
+                                         { hc_fetcher->afterSegmentNacked();
+                                         });
+
+  m_fetcher->onInOrderData.connect([hc_fetcher] (const ConstBufferPtr& buffer)
+                                         { hc_fetcher->onInOrderData(buffer);
+                                         });
+
+  m_fetcher->onInOrderComplete.connect([hc_fetcher] ()
+                                         { hc_fetcher->onInOrderComplete();
+                                         });
+
+  m_fetcher->afterSegmentValidated.connect([hc_fetcher] (const Data& data)
+                                         { hc_fetcher->afterValidationSuccess(data);
+                                         });
+
+  m_fetcher->afterSegmentTimedOut.connect([hc_fetcher] ()
+                                         { hc_fetcher->afterSegmentTimedOut();
+                                         });
+
+  m_fetcher->onError.connect([hc_fetcher] (uint32_t errorCode, const std::string& errorMsg)
+                                         { hc_fetcher->onError(errorCode, errorMsg);
+                                         });
+
+  hc_fetcher->m_fetcher = m_fetcher;
+
+  return hc_fetcher;
+}
+
+void
+HCSegmentFetcher::afterValidationSuccess(const Data& data) {
+  int segment_no = data.getName().get(-1).toSegment();
+  auto content = data.getContent();
+  content.parse();
+
+  if(data_map.size() != 0 && data_map.find(segment_no+1)->second != 0) {
+    auto cData = data_map.find(segment_no+1)->second;
+    auto m_content = cData->getContent();
+    m_content.parse();
+    auto hash_block = m_content.get(tlv::SignatureValue);
+    if (memcmp((void*)content.get(tlv::SignatureValue).value(), (void*)hash_block.value(), data.getSignatureValue().value_size())) {
+      onError(HASHCHAIN_ERROR, "Failure hash key error");
+    } else {
+      data_map.erase(segment_no+1);
+      afterSegmentValidated(data);
+    }
+  } else {
+    for (auto iter = content.elements_begin(); iter != content.elements_end(); iter++) {
+      if(iter->type() == tlv::SignatureValue) {
+        auto signature_block = iter.base();
+        std::shared_ptr<Block> block = std::make_shared<Block>(*signature_block); 
+        nextHash_map.insert(std::pair<int, std::shared_ptr<Block>>(segment_no, block));
+      }
+    }
+  }
+
+  if(segment_no != 0){
+    auto hash_block = nextHash_map.find(segment_no-1)->second;
+    if(hash_block == 0) {
+      std::shared_ptr<Data> m_data = std::make_shared<Data>(data); 
+      data_map.insert(std::pair<int, std::shared_ptr<Data>>(segment_no, m_data));
+    } else if (memcmp((void*)data.getSignatureValue().value(), (void*)hash_block->value(), data.getSignatureValue().value_size())) {
+      onError(HASHCHAIN_ERROR, "Failure hash key error");
+    } else {
+      data_map.erase(segment_no-1);
+      afterSegmentValidated(data);
+    }
+  } else {
+    afterSegmentValidated(data);
+  }
+}
+
+void
+HCSegmentFetcher::stop() {
+  if (!m_fetcher) {
+    return;
+  }
+
+  m_fetcher->stop();
+}
+}
+}
